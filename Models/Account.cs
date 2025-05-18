7f50eac391a23b53bab5770be384773bb145f565
ProgrammerNoob <sungshigun@gmail.com>
2025-05-19 01:30:37 +0800

    Update project files and enhance Account model
    - Updated `Bank-Attemp-Final.sln` for Visual Studio compatibility.

--- Models/Account.cs
+++ Models/Account.cs
@@ -10,11 +10,14 @@ namespace Models
         [Key]
         public Guid Id { get; set; }
         public string AccountId { get; set; }
-        public string CostumerName { get; set; } 
-        public string CostumerEmail { get; set; } 
+        public string CostumerName { get; set; }
+        public string CostumerEmail { get; set; }
         public string? Gender { get; set; }
         public DateTime? BirthDay { get; set; }
         public decimal? CurrentBalance { get; set; }
-        public DateTime CreatedAt { get; set; 
+        public DateTime CreatedAt
+        {
+            get; set;
+        }
     }
 }
