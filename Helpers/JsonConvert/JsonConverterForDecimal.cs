ca9d27d330bd09d573f8309ac0a0d515ac877217
ProgrammerNoob <sungshigun@gmail.com>
2025-05-21 03:54:44 +0800

    Add JSON converters for Decimal and DateTime types
    This commit introduces two new JSON converter classes:
    `JsonConverterForDecimal` for handling decimal serialization
    and deserialization, and `JsonDateTimeConverter` for
    converting DateTime objects. Both classes leverage the
    `System.Text.Json` namespace, offering custom formatting
    and parsing capabilities for their respective data types.

---
+++ Helpers/JsonConvert/JsonConverterForDecimal.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace HelperContainer.JsonConvert
+{
+    public class JsonConverterForDecimal:JsonConverter<Decimal>
+    {
+        public override decimal Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return reader.GetDecimal(); // or parse string if needed
+        }
+
+        public override void Write(Utf8JsonWriter writer, decimal value, JsonSerializerOptions options)
+        {
+            writer.WriteNumberValue(value); // or format if needed
+        }
+    }
+}
